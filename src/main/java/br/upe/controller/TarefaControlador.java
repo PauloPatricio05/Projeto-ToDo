51bea9d6b36ac6fc8bba8e441974eb346532140e
Adriel <adrielrodrigues156@gmail.com>
2025-12-01 20:08:26 -0300

    Adriel

--- src/main/java/br/upe/controller/TarefaControlador.java
+++ src/main/java/br/upe/controller/TarefaControlador.java
@@ -21,6 +21,15 @@ public class TarefaControlador {
     public void adicionarTarefaAtiva(Tarefa tarefa) {
         this.tarefaTableModel.getTarefasAtivas().add(tarefa);
     }
+    //Remover uma tarefa
+    public void removerTarefa(Tarefa tarefa){
+        System.out.println("3. Controlador recebeu o pedido para remover.");
+        boolean removeu = this.tarefaTableModel.getTarefasAtivas().remove(tarefa);
+        System.out.println("4. Conseguiu remover da lista? " + removeu);
+
+        this.tarefaTableModel.fireTableDataChanged();
+        System.out.println("5. Tabela avisada."); // Aqui mandamos um "Aviso" para a tabela dizendo : Refaça tudo. (Obs: Esse método vem direto da class AbstractTableModel)
+    }
 
     public void exibirFinalizadas(boolean exibir) {
         tarefaTableModel.setExibirFinalizadas(exibir);
