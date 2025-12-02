25c8a243005630a202ad3c242fe84e9efe6dca28
Adriel <adrielrodrigues156@gmail.com>
2025-12-02 11:26:13 -0300

    Interface_Erro

--- src/main/java/br/upe/ui/TelaPrincipal.java
+++ src/main/java/br/upe/ui/TelaPrincipal.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.ErrorManager;
 
 public class TelaPrincipal {
     private JPanel pnlMain;
